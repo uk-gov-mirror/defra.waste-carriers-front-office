aaf2c56f1753d358cb4b52124ac509e190d4826a
irisfaraway <iris.faraway@gmail.com>
2018-06-26 15:27:51 +0100

    Add support for logging Airbrake and Errbit errors
    As a standard we incorporate Airbrake in our apps to automatically send reports of any exceptions to an error logging system.
    
    [Airbrake](https://airbrake.io/) set the spec for the error API, and provide a gem that when added to a rails app automatically catches any exceptions raised. Typically it would then send that to Airbrake itself, but as that is a paid for service we instead send it to an instance of [Errbit](https://github.com/errbit/errbit) we manage ourselves.

--- config/application.rb
+++ config/application.rb
@@ -28,5 +28,19 @@ module WasteCarriersFrontOffice
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Errbit config - If you are confused why we say this is Errbit config then
+    # name everything Airbrake, its because Airbrake (the company) set the
+    # API specification and provide the gem, but we choose instead to send
+    # notifications to an instance of Errbit we manage ourselves
+    # (cause its free!)
+    config.airbrake_on = ENV["EMS_USE_AIRBRAKE"] == "true" ? true : false
+    config.airbrake_host = ENV["EMS_AIRBRAKE_HOST"]
+    # Even though we may not want to enable airbrake, its initializer requires
+    # a value for project ID and key else it errors. So to simplify local
+    # development we default these values to save having to set anything in the
+    # .env file.
+    config.airbrake_id = ENV["EMS_AIRBRAKE_PROJECT_ID"] || 1
+    config.airbrake_key = ENV["EMS_AIRBRAKE_PROJECT_KEY"] || "dummy"
   end
 end
