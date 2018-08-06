2ce4b3f8451fdde739a7b9d6de2c1581df707319
Iris Faraway <iris.faraway@gmail.com>
2018-08-06 11:44:06 +0100

    Set config value for metaData.route (#37)
    https://eaflood.atlassian.net/browse/WC-123
    
    Update the application config to include the value we want to save for metaData.route.
    
    See https://github.com/DEFRA/waste-carriers-renewals/pull/236 for details.

--- config/application.rb
+++ config/application.rb
@@ -96,6 +96,9 @@ module WasteCarriersFrontOffice
     config.worldpay_password = ENV["WCRS_WORLDPAY_ECOM_PASSWORD"]
     config.worldpay_macsecret =  ENV["WCRS_WORLDPAY_ECOM_MACSECRET"]
 
+    # Digital or assisted digital metaData.route value
+    config.metadata_route = "DIGITAL"
+
     # Version info
     config.application_version = "0.0.1".freeze
     config.application_name = "waste-carriers-front-office"
